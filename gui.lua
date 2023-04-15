023e8418c655696779367da623b92fc62ff6bca2
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-04-15 15:48:41 +0200

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -3,12 +3,29 @@ local w, h = term.getSize()
 -- Couleurs
 local bgColor = colors.blue
 local fgColor = colors.white
+local iconColor = colors.red
 
--- Dessiner le fond
-term.setBackgroundColor(bgColor)
-term.clear()
+while true do
+  -- Dessiner le fond
+  term.setBackgroundColor(bgColor)
+  term.clear()
 
--- Dessiner la barre en bas
-term.setCursorPos(1, h)
-term.setBackgroundColor(fgColor)
-term.write(string.rep(" ", w))
+  -- Dessiner l'icône de reboot
+  term.setCursorPos(1, h)
+  term.setBackgroundColor(iconColor)
+  term.write(" ")
+
+  -- Dessiner la barre en bas
+  term.setCursorPos(2, h)
+  term.setBackgroundColor(fgColor)
+  term.write(string.rep(" ", w-1))
+
+  -- Attendre un clic sur l'icône de reboot
+  local _, y = os.pullEvent("mouse_click")
+  if y == h then
+    shell.run("reboot")
+  end
+
+  -- Attendre un peu avant de recommencer
+  sleep(0.1)
+end
